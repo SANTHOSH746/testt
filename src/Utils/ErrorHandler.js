21ea1328e7ac5e6daf1024bfc94809835533bd85
Jishnu894 <vjishnukumar@gmail.com>
2025-01-23 12:22:32 +0530

    massy

---
+++ src/Utils/ErrorHandler.js
@@ -0,0 +1,8 @@
+class ErrorHandler extends Error {
+    constructor(statusCode, message) {
+        super(message);
+        this.statsuCode = statusCode;
+    }
+}
+
+module.exports = ErrorHandler;
